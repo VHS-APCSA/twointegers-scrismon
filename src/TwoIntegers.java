429ed140bde5e6cd20727efd47d6cd79bc985012
bob <bob@APCSA>
2017-09-21 14:49:10 -0700

    Added object, constructer header

---
+++ src/TwoIntegers.java
@@ -0,0 +1,16 @@
+
+public class TwoIntegers 
+{
+	private int integer1;
+	private int integer2;
+	
+	public TwoIntegers();
+	{
+		this.integer1 = 0;
+		this.integer2 = 0;
+	}
+	public TwoIntegers(int integer1, int integer2)
+	{
+		
+	}
+}
